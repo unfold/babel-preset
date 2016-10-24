8dfcbb3ce1d1ecc44ead6a042e6aca47f1fd53da
Einar Löve <einar.love@gmail.com>
2016-10-24 13:04:19 +0200

    Fix linting script and linting errors

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ module.exports = {
   presets: [
     'babel-preset-es2015',
     'babel-preset-stage-0',
-    'babel-preset-react'
+    'babel-preset-react',
   ].map(require.resolve),
 
   plugins: [
     'babel-plugin-add-module-exports',
-    'babel-plugin-lodash'
-  ].map(require.resolve)
+    'babel-plugin-lodash',
+  ].map(require.resolve),
 }
